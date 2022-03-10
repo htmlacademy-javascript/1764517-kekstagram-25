87fe708cd28f01d3b0af1ddae547dd9b63aaf256
Keks <keks@htmlacademy.ru>
2022-03-10 10:55:24 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/miniature.js
@@ -0,0 +1,18 @@
+import {generatePhotos} from './data.js';
+
+const similarListElement = document.querySelector('.pictures');
+const similarPhotoTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const similarPhotos = generatePhotos();
+
+const similarListFragment = document.createDocumentFragment();
+
+similarPhotos.forEach(({url, likes, comment}) => {
+  const userPhoto = similarPhotoTemplate.cloneNode(true);
+  userPhoto.querySelector('.picture__img').src = url;
+  userPhoto.querySelector('.picture__likes').textContent = likes;
+  userPhoto.querySelector('.picture__comments').textContent = comment.length;
+  similarListFragment.appendChild(userPhoto);
+});
+
+similarListElement.appendChild(similarListFragment);
