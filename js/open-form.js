585e54b4d117d357cda9097cb58768f4705838f5
Evgeniy <nikolaev_zheka-zik@bk.ru>
2022-03-17 16:36:32 +0300

    -

--- js/open-form.js
+++ js/open-form.js
@@ -1,11 +1,11 @@
 import {isEscapeKey} from './util.js';
 
-
 const uploadFile = document.getElementById('upload-file');
 const uploadOverlay = document.querySelector('.img-upload__overlay');
 const closeFormButton = document.getElementById('upload-cancel');
 const body = document.querySelector('body');
-const textContent = document.querySelector('.img-upload__text');
+const hashtags = document.querySelector('.text__hashtags');
+const description = document.querySelector('.text__description');
 const scaleValue = document.querySelector('.scale__control');
 
 const openForm = () => {
@@ -13,13 +13,15 @@ const openForm = () => {
     uploadOverlay.classList.remove('hidden');
     body.classList.add('modal-open');
   });
+  // const space = ' ';
+  // const arrayHashtags = hashtags.value.split(space);
 
   const closeForm = () => {
     uploadOverlay.classList.add('hidden');
     body.classList.remove('modal-open');
     uploadFile.value = '';
-    textContent.querySelector('.text__hashtags').value = '';
-    textContent.querySelector('.text__description').value = '';
+    hashtags.value = '';
+    description.value = '';
     scaleValue.value = '55%';
   };
 
